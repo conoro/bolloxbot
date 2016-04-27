13f55f5b63e431ce568d233152993590efabc094
Conor ONeill <conor@conoroneill.com>
2016-04-27 13:08:18 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bolloxbot
-Facebook Messenger Bot that says bollox to whatever you type
+This repository contains a simple bolloxing Facebook Messenger bot forked from https://github.com/giannoug/facebook-messenger-bot using instructions from http://hashbang.gr/your-own-facebook-messenger-bot-powered-by-nodejs/ and https://developers.facebook.com/docs/messenger-platform/quickstart
---
+++ index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+config = require(__dirname + '/config.json');
+
+var verify_token = config.verify_token;
+var token = config.token; 
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/webhook/', function (req, res) {
+
+    if (req.query['hub.verify_token'] === verify_token) {
+        res.send(req.query['hub.challenge']);
+    }
+
+    res.send('Error, wrong validation token');
+
+});
+
+app.post('/webhook/', function (req, res) {
+
+    var messaging_events = req.body.entry[0].messaging;
+
+    for (var i = 0; i < messaging_events.length; i++) {
+
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+
+            sendTextMessage(sender, "Bollox");
+        }
+    }
+
+    res.sendStatus(200);
+
+});
+
+app.listen(1337, function () {
+
+    console.log('Facebook Messenger echoing bot started on port 1337!');
+
+});
+
+function sendTextMessage(sender, text) {
+
+    var messageData = {
+        text: text
+    };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response) {
+
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+
+    });
+
+}
